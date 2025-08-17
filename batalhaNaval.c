b97ee9d00e43d88a8bd3db7409399c1ddf9662d8
Yuri Oliveira <ysoli7777@gmail.com>
2025-08-17 11:37:13 -0300

    Atualizando arquivo do Batalha Naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,37 +4,54 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+#include <stdio.h>
+
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int n[5][5] = {0};
+    n[1][2] = 3; n[2][2] = 3; n[3][2] = 3;
+    n[0][0] = 3; n[0][1] = 3; n[0][2] = 3;
+    printf("Novato:\n");
+    for(int i = 0; i < 5; i++)
+        for(int j = 0; j < 5; j++)
+            if(n[i][j] == 3)
+                printf("(%d,%d)\n", i, j);
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    int a[10][10] = {0};
+    a[1][1] = 3; a[2][2] = 3; a[3][3] = 3;
+    a[0][0] = 3; a[0][1] = 3; a[0][2] = 3;
+    a[5][0] = 3; a[5][1] = 3; a[5][2] = 3;
+    a[7][2] = 3; a[6][3] = 3; a[5][4] = 3;
+    printf("\nAventureiro:\n");
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++)
+            printf("%d ", a[i][j]);
+        printf("\n");
+    }
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    int cone[3][5] = {{0,0,1,0,0},{0,1,1,1,0},{1,1,1,1,1}};
+    int cruz[3][5] = {{0,0,1,0,0},{1,1,1,1,1},{0,0,1,0,0}};
+    int oct[3][5] = {{0,0,1,0,0},{0,1,1,1,0},{0,0,1,0,0}};
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    printf("\nMestre - Cone:\n");
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 5; j++)
+            printf("%d ", cone[i][j]);
+        printf("\n");
+    }
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    printf("\nMestre - Cruz:\n");
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 5; j++)
+            printf("%d ", cruz[i][j]);
+        printf("\n");
+    }
+
+    printf("\nMestre - Octaedro:\n");
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 5; j++)
+            printf("%d ", oct[i][j]);
+        printf("\n");
+    }
 
     return 0;
 }
